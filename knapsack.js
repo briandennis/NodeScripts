52580a0df5c8979d92dedab2e70c6a1726cb75f2
Brian Dennis <briandennis321@gmail.com>
2016-03-12 22:10:35 -0600

    added knapsack stuff

---
+++ knapsack.js
@@ -0,0 +1,26 @@
+var sackItems = require('./sackItems.js');
+var items = sackItems.items;
+
+function packSack(sackItems, packCapacity){
+  var map = [];
+
+  function opt (index, remainingCapacity){
+    if(index >= sackItems.length || remainingCapacity <= 0) return 0;
+    else{
+      if(map[index] === undefined){
+        map[index] = [];
+      }
+      else if(map[index][remainingCapacity]) return map[index][remainingCapacity];
+      var best = Math.max(
+        (sackItems[index][0] + opt(index + 1, remainingCapacity - sackItems[index][1])),
+        opt(index + 1, remainingCapacity)
+      );
+      map[index][remainingCapacity] = best;
+      return best;
+    }
+  }
+
+  return opt(0, packCapacity);
+}
+
+console.log('Final value: ' + packSack(items, 70));
---
+++ sackItems.js
@@ -0,0 +1,4 @@
+var exports = module.exports = {};
+
+// [value, weight]
+exports.items = [[1,5],[2,7],[1,3],[3,6]];
