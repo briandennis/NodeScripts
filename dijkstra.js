4753be09cd48258817ac5eb6b40c33ee08edab72
Brian Dennis <briandennis321@gmail.com>
2016-03-13 14:45:03 -0500

    finished dijkstra, just need to test

--- dijkstra.js
+++ dijkstra.js
@@ -14,10 +14,12 @@ function Graph (nodes){
 
 function dijkstra(v,u, graph){
   var queue = [v];
+
   var visited = [v];
 
   var nodeInfo = new Map();
 
+  //initialize nodeInfo with all nodes
   graph.nodes.forEach(function(item){
     if(item !== v){
       nodeInfo.set(item, [null, Infinity]);
@@ -28,16 +30,49 @@ function dijkstra(v,u, graph){
     }
   });
 
+  //spoofed, actually takes O(n) time, but could be O(log n) if
+  // a heap or real binary search algorithm was used
+  function binaryInsert(node){
+    var nodeWeight = nodeInfo.get(node)[1];
+    var wasAdded = false;
+    for(var i = 0; i < queue.length; i++){
+      if(nodeInfo.get(queue[i])[1] > nodeWeight){
+        queue.splice(i, 0, node);
+        break;
+      }
+    }
+    if(!wasAdded) queue.push(node);
+  }
+
+  //how to handle every edge
   var handleNode = function(destinationNode, weight){
     if(!visited.includes(destinationNode)){
       if(!queue.includes(destinationNode)){
-
+        nodeInfo.set(destinationNode, [currNode,currDistance + weight]);
+        binaryInsert(destinationNode);
+      }
+      else{
+        if(nodeInfo.get(destinationNode)[1] > currDistance + weight){
+          queue.remove(destinationNode);
+          nodeInfo.set(destinationNode, [nodeInfo.get(destinationNode)[0], currDistance + weight]);
+          binaryInsert(destinationNode);
+        }
       }
     }
   };
 
   while(queue.length > 0){
     var currNode = queue.shift();
+    visited.push(currNode);
+    var currDistance = nodeInfo.get(currNode);
     currNode.edges.forEach(handleNode);
   }
+
+  var backTraceNode = u;
+  console.log(u.value);
+  while(nodeInfo.get(backTraceNode)[0] !== null){
+    backTraceNode = nodeInfo.get(backTraceNode)[0];
+    console.log(backTraceNode.value);
+  }
+  return nodeInfo.get(u)[1];
 }
