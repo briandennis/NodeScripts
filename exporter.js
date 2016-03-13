89e70e39f1e4abb908dfe273e5dca5fafaa8b39c
Brian Dennis <briandennis321@gmail.com>
2016-03-13 16:23:17 -0500

    added inerval export

--- exporter.js
+++ exporter.js
@@ -2,3 +2,6 @@ var exports = module.exports = {};
 
 // [value, weight]
 exports.sackItems = [[1,5],[2,7],[1,3],[3,6]];
+
+//inervals, used for inerval scheduling app
+exports.intervals = [[0,3],[5,9],[0,5],[2,4],[7,9],[9,14],[10,11],[3,6],[7,8],[12,15]];
