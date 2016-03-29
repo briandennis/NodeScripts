113228c4c4d4de87c1f54227f1faa34367d47431
Brian Dennis <briandennis321@gmail.com>
2016-03-29 13:59:35 -0500

    added more, still buggy

--- quicksort.js
+++ quicksort.js
@@ -4,14 +4,62 @@ var list = imports.mergeList;
 
 function quickSort(list){
 
-  var lessList, greaterList = [];
+  function partition(start, end){
 
-  while(lessList.length < list.length/4 || greaterList.length < list.length/4){
-    var pivotIndex = Math.floor(Math.random()*list.length-1);
-    list.forEach(function(item, index){
-      if(index !== pivotIndex){
-        if(item < list[pivotIndex])
+    console.log("STARTING SWAP from " + start + " to " + end-1 +  "\n");
+
+    //pick pivot
+    var pivotIndex = start;
+    var partitionLength = end - start;
+
+    //return if base
+    if(partitionLength <= 1) return list;
+
+    while(pivotIndex > partitionLength/4 + partitionLength/2 || pivotIndex < partitionLength/4){
+      pivotIndex = Math.floor(Math.random()*list.length-1);
+    }
+    console.log('pivot index: ' + pivotIndex);
+
+    var pivotVal = list[pivotIndex];
+    var right = end-1, left = start;
+
+
+    //swap pivot
+    list[pivotIndex] = list[right];
+    list[right] = pivotVal;
+
+    console.log("STARTING SWAP from " + left + " to " + right +  "\n");
+    //swap until done
+    while(left < right){
+      if(list[left] > pivotVal){
+        var tmp = list[right];
+        list[right] = list[left];
+        list[left] = tmp;
+        right--;
       }
-    });
+      else{
+        left++;
+      }
+    }
+
+    if(list[left] >= pivotVal){
+      list[end-1] = list[left];
+      list[left] = pivotVal;
+    }
+    else if(left + 1 < end-1){
+      list[end-1] = list[left+1];
+      list[left+1] = pivotVal;
+    }
+
+    console.log(list);
+
+    partition(start,left);
+    partition(left+1,end);
+
+    return list;
   }
+
+  return partition(0, list.length);
 }
+
+console.log(quickSort(list));
